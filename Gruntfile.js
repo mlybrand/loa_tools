dcc129a578575d5c0939667e9803c23aed3e4e92
Mark Lybrand <mlybrand@gmail.com>
2015-10-05 21:13:04 -0700

    added aliases file to further modularize grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,5 @@
     module.exports = function(grunt) {
         require('load-grunt-config')(grunt);
 
-        grunt.registerTask('default', function() {
-            console.log('I am grunt!');
-        });
     };
 }());
