0ec51a70e6a84d478ab9163c9fe1e321f928aa0b
Mark Lybrand <mlybrand@gmail.com>
2015-10-05 23:59:57 -0700

    added server and app files for node express app

---
+++ src/server/app.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    var start = function () {
+        console.log('starting');
+    };
+
+    var stop = function () {
+        console.log('stopping');
+    };
+
+    module.exports = {
+        start: start,
+        stop: stop
+    };
+}());
---
+++ src/server/server.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+
+    var app = require('./app');
+
+    app.start();
+    app.stop();
+}());
