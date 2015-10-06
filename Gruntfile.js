91f343d05b905a1b300d3d72b1d5ec9d71ad314f
Mark Lybrand <mlybrand@gmail.com>
2015-10-05 20:57:03 -0700

    update jshintrc to use strict (but loosened for node)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,11 @@
-module.exports = function(grunt) {
-    require('load-grunt-config')(grunt);
+(function() {
+    'use strict';
 
-    grunt.registerTask('default', function() {
-        console.log('I am grunt!');
-    });
-};
+    module.exports = function(grunt) {
+        require('load-grunt-config')(grunt);
+
+        grunt.registerTask('default', function() {
+            console.log('I am grunt!');
+        });
+    };
+}());
