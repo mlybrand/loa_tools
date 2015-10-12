a82b58e1d26b020330e60162392aa7b199ac4436
Mark Lybrand <mlybrand@gmail.com>
2015-10-11 22:51:44 -0700

    added e2e protractor config to do multiCapabilities of both chrome and ff

--- grunt/protractor.js
+++ grunt/protractor.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     e2e: {
         options: {
-            configFile: 'test/config/protractor/ci.conf.js',
+            configFile: 'test/config/protractor/e2e.conf.js',
             keepAlive: false
         }
     },
---
+++ test/config/protractor/e2e.conf.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    var specFiles = require('./../../includes/protractorSpecs'),
+        baseUrl = require('./../../includes/baseUrl');
+
+    exports.config = {
+        specs: specFiles,
+        baseUrl: baseUrl,
+        multiCapabilities: [
+            {
+                browserName: 'firefox'
+            },
+            {
+                browserName: 'chrome'
+            }
+        ]
+    };
+}());
