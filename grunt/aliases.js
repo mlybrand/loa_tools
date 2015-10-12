9507cd0b86d703e464063f284101526d1abdac30
Mark Lybrand <mlybrand@gmail.com>
2015-10-12 00:39:49 -0700

    added mocha/chai for unit tests of server code. configured to be part of ci build

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -2,15 +2,26 @@ module.exports = {
     'default': [
         'jshint',
         'jscs',
+        'test:unit-server-single',
         'test:e2e-single'
     ],
     'ci': [
         'jshint',
         'jscs',
+        'test:unit-server-ci',
         'test:e2e-ci'
     ],
     'dev': [
         'default'
+    ],
+    'test:unit-server-single': [
+        'mochaTest'
+    ],
+    'test:unit-server-ci': [
+        'mochaTest'
+    ],
+    'test:unit-server-continuous': [
+        'mochaTest'
     ],
     'test:e2e-single': [
         'run:dev',
---
+++ test/server/first.spec.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    var expect = require('chai').expect;
+
+    describe('fake test', function() {
+        it('should fail', function() {
+            expect(true).to.be.true;
+        });
+    });
+}());
