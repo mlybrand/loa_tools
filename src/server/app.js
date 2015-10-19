11d30d8c41de8823a2bc13de2f317749b54b960c
Mark Lybrand <mlybrand@gmail.com>
2015-10-19 07:57:48 -0700

    added staticFileMaps to convict config to allow dev to point to resources anywhere, while being able to restrict this in prod

--- src/server/app.js
+++ src/server/app.js
@@ -2,15 +2,23 @@
     'use strict';
 
     var appFactory = function(config) {
-        console.log(config.get('env'));
         var start = function(callback) {
             var express = require('express'),
                 app = express(),
-                server = null;
+                server = null,
+                staticFileMaps = config.get('staticFileMaps');
+
+            for (var i = 0, len = staticFileMaps.length; i < len; i++) {
+                staticFileMaps[i].directoryName =
+                    staticFileMaps[i].directoryName || '/';
+                app.use(staticFileMaps[i].directoryName,
+                    express.static(__dirname + staticFileMaps[i].filePath));
+            }
 
             app.get('/', function(req, res) {
                 var head = '<head><title>Hello World</title></head>',
-                    body = '<body><h1>Hello World</h1></body>',
+                    para = '<p>' + staticFileMaps + '</p>',
+                    body = '<body><h1>Hello World</h1>' + para + '</body>',
                     page = '<html>' + head + body + '</html>';
                 res.send(page);
             });
--- src/server/config.js
+++ src/server/config.js
@@ -2,21 +2,52 @@
     'use strict';
 
     var convict = require('convict'),
-        config = convict({
-            env: {
-                doc: 'The application environment',
-                format: ['development', 'production'],
-                default: 'development',
-                env: 'NODE_ENV'
-            },
-            port: {
-                doc: '',
-                format: 'port',
-                default: 3000,
-                env: 'PORT'
+        config =  null,
+        env = null;
+
+    convict.addFormat({
+        name: 'static-file-maps',
+        validate: function(val) {
+            if (Object.prototype.toString.call(val) !==
+                    '[object Array]') {
+                throw new Error('must be an array');
             }
-        }),
-        env = config.get('env');
+            for (var i = 0, len = val.length; i < len; i ++) {
+                if (typeof val !== 'object') {
+                    throw new Error('must be an array of objects');
+                }
+                if (!val[i].filePath) {
+                    throw new Error(
+                        'each object must have a filePath property');
+                }
+                if (typeof val[i].filePath !== 'string') {
+                    throw new Error('filePath must be a string');
+                }
+                // no test for val.directoryName as it can be null
+            }
+        }
+    });
+
+    config = convict({
+        env: {
+            doc: 'The application environment',
+            format: ['development', 'production'],
+            default: 'development',
+            env: 'NODE_ENV'
+        },
+        port: {
+            doc: '',
+            format: 'port',
+            default: 3000,
+            env: 'PORT'
+        },
+        staticFileMaps: {
+            doc: 'Array of objects { filePath:"directoryName" }',
+            format: 'static-file-maps',
+            default: [{filePath: 'public', directoryName: null}]
+        }
+    });
+    env = config.get('env');
 
     config.loadFile(__dirname + '/config/' + env + '.json');
     config.validate({strict: true});
